8eed499ce9e2b6d29f86734a296b9f3d970da832
mpm86 <74258852+mpm86@users.noreply.github.com>
2023-10-23 23:30:02 -0400

    Update Cylinder.h

--- Cylinder.h
+++ Cylinder.h
@@ -1,5 +1,3 @@
-// Modified Cylinder.h
-
 #ifndef CYLINDER_H
 #define CYLINDER_H
 
@@ -8,21 +6,21 @@
 
 class Cylinder : public Polyhedron {
 private:
-    double _radius;  // Changed from radius to _radius
-    double _height;  // Changed from height to _height
+  double _radius;
+  double _height;
 
 public:
-    // Constructors
-    Cylinder();
-    Cylinder(double radius, double height);
+  // Constructors
+  Cylinder();
+  Cylinder(double radius, double height);
 
-    // Member functions
-    void setRadius(double radius);
-    void setHeight(double height);
-    void display() const;
-    void read(std::istream& ins);
-    Polyhedron* clone() const;
-    void scale(double scalingFactor);
+  // Member functions
+  void setRadius(double radius);
+  void setHeight(double height);
+  void display() const;
+  void read(std::istream& ins);
+  Polyhedron* clone() const;
+  void scale(double scalingFactor);
 };
 
 #endif
